7aad08915d65d229df52672b0ca1601cf1463aa8
ashish96082 <ashish96082@gmail.com>
2018-02-04 22:50:44 +0530

    + Data transfer between server and app is now encrypted. + Video is played on clicking start course.

--- app/src/main/java/com/example/piyush/magicalmethods/CourseDetail.java
+++ app/src/main/java/com/example/piyush/magicalmethods/CourseDetail.java
@@ -1,11 +1,13 @@
 package com.example.piyush.magicalmethods;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by USER
@@ -26,7 +28,18 @@ public class CourseDetail extends AppCompatActivity {
         tocHeading=findViewById(R.id.tocHeading);
         button=findViewById(R.id.button);
         MyData myData = getIntent().getParcelableExtra("Card");
-        ActivityCourseDetailBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_course_detail);
+        ActivityCourseDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_course_detail);
+
         binding.setMyData(myData);
+        binding.setPresenter(this);
+    }
+
+    void openVideoPlayerActivity() {
+        Intent intent = new Intent(CourseDetail.this, VideoPlayerActivity.class);
+        startActivity(intent);
+    }
+
+    public void onStartCourseClick(View view) {
+        openVideoPlayerActivity();
     }
 }
